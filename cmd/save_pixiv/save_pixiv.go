4bc774fc310ac46e23f3c684d17aae768066c26b
Osamu Koga <osak.63@gmail.com>
2019-12-14 08:10:42 -0600

    Restore debug comment out

--- cmd/save_pixiv/save_pixiv.go
+++ cmd/save_pixiv/save_pixiv.go
@@ -4,6 +4,7 @@ import (
 	"github.com/osak/Akasha-Chronik/internal/config"
 	"github.com/osak/Akasha-Chronik/internal/pixiv"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -23,20 +24,18 @@ func main() {
 		panic(err)
 	}
 
-	/*
-		startPage := 1
-		if len(os.Args) >= 3 {
-			startPage, err = strconv.Atoi(os.Args[2])
-			if err != nil {
-				panic(err)
-			}
-		}
-
-		err = saver.SaveBookmarks(startPage)
+	startPage := 1
+	if len(os.Args) >= 3 {
+		startPage, err = strconv.Atoi(os.Args[2])
 		if err != nil {
 			panic(err)
 		}
-	*/
+	}
+
+	err = saver.SaveBookmarks(startPage)
+	if err != nil {
+		panic(err)
+	}
 
 	err = saver.SaveBookmarksNovel(1)
 	if err != nil {
